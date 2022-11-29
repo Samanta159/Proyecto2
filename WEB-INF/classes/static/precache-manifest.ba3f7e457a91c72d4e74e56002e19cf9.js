c448eb0b2b4039fb2704fda012d071db9f7edc19
PC-RJB-2022\RaulJBPcNegra <rbenitezy@gmail.com>
2022-11-29 10:26:53 -0600

    first commit

---
+++ WEB-INF/classes/static/precache-manifest.ba3f7e457a91c72d4e74e56002e19cf9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9ee8037eb839661c8d50ab01ed5266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7447372560c9d3ea27f7",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "aacb77bd9778b7c04c5a",
+    "url": "/static/css/main.22d2b07a.chunk.css"
+  },
+  {
+    "revision": "7447372560c9d3ea27f7",
+    "url": "/static/js/2.1067fce7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1067fce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aacb77bd9778b7c04c5a",
+    "url": "/static/js/main.9043904d.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
